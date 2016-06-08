47c4625c16164e649da71e63fa24c157f1d52cd0
gaoqiankun <gqk007@gmail.com>
2016-06-08 10:04:16 +0800

    * init script optimization

--- init.sh
+++ init.sh
@@ -1,31 +1,35 @@
 dir=$(cd `dirname $0`;pwd)
 home=$(cd;pwd)
-# 先检测本目录下是否存在vimrc文件 不存在创建一个
+# init vimrc if not exist
 if [ ! -f "$dir/vimrc" ];then
     touch "vimrc"
 fi
 
-# 软链接home目录的.vimrc 到 本目录下的vimrc文件
+# link vimrc to ~/.vimrc
 ln -sf "$dir/vimrc" ~/.vimrc
 
-# 如果home目录已经存在 .vim目录 先删除
-if [ -d "$home/.vim/bundle/Vundle.vim " ];then
-    rm -rf "$home/.vim/bundle/Vundle.vim"
+# clear old ~/.vim if exist or create it
+if [ -d "$home/.vim" ];then
+    rm -rf "$home/.vim/*"
+else
+    mkdir "$home/.vim"
 fi
 
 # link conf to ~/.vim/conf
 ln -sf "$dir/conf" ~/.vim/
 
-# 检测是否已经安装git 未安装应该提示安装git后手动安装vundle
+# git not found waring
 if [ ! -n `which git` ];then
-    echo "未安装git,请安装git后手动执行: "
-    echo "  git clone https://github.com/VundleVim/Vundle.vim.git $home/.vim/bundle/Vundle.vim"
+    echo "git not fond, install git first please."
     exit
 fi
 
-# 安装vundle
+# install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git $home/.vim/bundle/Vundle.vim
 
+# install plugins
 vim +PluginInstall +qall
+
+# every thing done
 echo ""
-echo "Everything done, enjoy Vim Vundle, ^-^.";
+echo "Everything done, ^-^.";
